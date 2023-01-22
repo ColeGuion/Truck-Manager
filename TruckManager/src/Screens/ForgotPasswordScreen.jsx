62a759fd751d9b3f5cb08a3938118906468aebc9
masonxotto <masonxotto@gmail.com>
2023-01-21 19:47:33 -0600

    added fetch requests to validate username and then update forgot password to backend. Also fixed a styling bug where the input field extended too far

--- TruckManager/src/Screens/ForgotPasswordScreen.jsx
+++ TruckManager/src/Screens/ForgotPasswordScreen.jsx
@@ -10,38 +10,36 @@ export default function ForgotPasswordScreen({ navigation }){
 /*TODO: write simple function to check for a valid usernamethen display a different 
         UI for validation (probably via email), then display reset password UI
 */
+const APIURL = "http://localhost:5000";
 const navigate = useNavigate();
 const [validatedUsername, setValidatedUsername] = useState(false);
+const [globalUsername, setGlobalUsername] = useState("");
+
 
 const ValidatingUsername = () => {
 
-  const [username, setUsername] = useState("");
   const [usernameError, setUsernameError] = useState("");
+  const [username, setUsername] = useState("");
 
   const validUsername = async (e) => {
     e.preventDefault();
-    if (username === "admin") { //this will eventually run against all username in database to see if there is a match
+    const response = await fetch(`${APIURL}/validusername/${username}`)
+    const validated = await response.json();
+    if(validated.response) {
       setValidatedUsername(true);
+      setGlobalUsername(username);
     }
     else {
+      setUsername("");
       setUsernameError("Not Valid Username");
     }
   }
 
-  const handleKeyDown = (e) => {
-    if(e.nativeEvent.key == "Enter"){
-      validUsername();
-    }
-  }
-
-
   return (
     <form className="form-fgt-pass" onSubmit={validUsername}>
       {usernameError.length > 0 && <div style={{color:'red', fontWeight:'bold', margin:'10px'}}>{usernameError}</div>}
-      <div className="softContainer">
         <label className="input-label" htmlFor="username">Username*</label>
         <input className="input-field" id="usename" onChange={(e) => setUsername(e.target.value)} value={username}/>
-      </div>
       <button className="login-btn">Forgot Password</button>
     </form>
   );
@@ -57,37 +55,47 @@ const ResetPassword = () => {
   const validPassword = async (e) => {
     e.preventDefault();
     if (newPassword === newPasswordConfirm) { //this will eventually run against pararameters for a password, such as 8 characters in length, number, etc. it will also ensure it is not the old password
-      setValidatedPassword(true);
-      setNewPassword("");
-      setNewPasswordConfirm("");
-      if (passwordError.length > 0) {
-        setPasswordError("");
+      const response = await fetch(`${APIURL}/forgotpassword`, {
+        method: 'PUT',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          username: globalUsername,
+          newPassword: newPassword
+        })
+      });
+      const res = await response.json()
+      console.log(res);
+      if(res.response) {
+        navigate("/");
       }
-      navigate("/");
+      else {
+        if(res.errcode === 1) {
+          setPasswordError("Password must have 8-32 characters, capital letter, and a number");
+        }
+        if(res.errcode === 2) {
+          setPasswordError("something went wrong try again");
+        }
+        setNewPassword("");
+        setNewPasswordConfirm("");
+      }
     }
     else {
       setPasswordError("Passwords do not match!");
+      setNewPassword("");
+      setNewPasswordConfirm("");
     }
   }
-
-  const handleKeyDown = (e) => {
-    if(e.nativeEvent.key == "Enter"){
-      validPassword();
-    }
-  }
-
 
   return (
     <form className="form-fgt-pass" onSubmit={validPassword}>
         {passwordError.length > 0 && <div style={{color:'red', fontWeight:'bold', margin:'10px'}}>{passwordError}</div>}
-        <div className="softContainer">
           <label className="input-label" htmlFor="newpassword">New Password</label>
           <input className="input-field" id="newpassword" onChange={(e) => setNewPassword(e.target.value)} value={newPassword} type="password"/>
-        </div>
-        <div className="softContainer">
           <label className="input-label" htmlFor="confriknewpassword">Confirm Password</label>
           <input className="input-field" id="confirmnewpassword" onChange={(e) => setNewPasswordConfirm(e.target.value)} value={newPasswordConfirm} type="password"/>
-        </div>
         <button className="login-btn">Reset Password</button>
     </form>
   );
