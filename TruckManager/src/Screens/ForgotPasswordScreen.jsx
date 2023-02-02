c5a180a620682ebc38e99af128439a3f910d5af8
masonxotto <masonxotto@gmail.com>
2023-02-02 15:12:01 -0600

    updated dependencies and refactored ForgotPasswordScreen to be cleaner and more performant

--- TruckManager/src/Screens/ForgotPasswordScreen.jsx
+++ TruckManager/src/Screens/ForgotPasswordScreen.jsx
@@ -6,142 +6,12 @@ import { useNavigate } from 'react-router-dom';
 import '../Styles/GlobalStyles.css';
 import '../Styles/ForgotPasswordScreen.css'
 
-/*-------------------------------------------------------------------------
-  Forgot Password Component
--------------------------------------------------------------------------*/
-export default function ForgotPasswordScreen({ navigation }){
-/*TODO: write simple function to check for a valid usernamethen display a different 
-        UI for validation (probably via email), then display reset password UI
-*/
 const APIURL = "http://localhost:5000";
-const navigate = useNavigate();
-const [validatedUsername, setValidatedUsername] = useState(false);
-const [globalUsername, setGlobalUsername] = useState("");
-const [isValidated, setIsValidated] = useState(false);
-const [emailAddress, setEmailAddress] = useState("");
-const [sentOTPEmail, setSentOTPEmail] = useState(false);
 
-/*
--Author: Mason Otto
--Last Modified: 1/23/2023
--Description: This is a React Component that is displayed to verify the OTP sent to the requesting user's email
--Return: HTML Code that allows the user to enter the OTP
-TODO: Test code and refactor
-*/ 
-const VerifyOTP = () => {
-  const [otpError, setOTPError] = useState("");
-  const [oneTimePin, setOneTimePin] = useState("");
-
-  /*
-  -Author: Mason Otto
-  -Last Modified: 1/23/2023
-  -Description: This fetches to the backend API to generate a OTP and get the users email address
-  -Return: N/A
-  TODO: Test code and refactor
-  */ 
-  async function getOTPEmail(){
-    const response = await fetch(`${APIURL}/requestotp`, {
-      method: 'PUT',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        username: globalUsername
-      })
-    });
-    const sentEmail = await response.json();
-    if(sentEmail.response === "EMAIL SENT") {
-      setSentOTPEmail(true);
-      setEmailAddress(sentEmail.email);
-    }
-  }
-  /*
-  -Author: Mason Otto
-  -Last Modified: 1/23/2023
-  -Description: This fetches to the backend API to verify that the input OTP is correct
-  -Return: N/A
-  TODO: Test code and refactor
-  */ 
-  async function validateOTP(e) {
-    e.preventDefault();
-    const response = await fetch(`${APIURL}/verifyotp`, {
-      method: 'PUT',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        username: globalUsername,
-        otp: oneTimePin,
-      })
-    });
-    const verified = await response.json();
-    if(verified.response === "VERIFIED") {
-      setIsValidated(true);
-    }
-    else {
-      setOTPError(`Invalid One Time Pin! Check email sent to ${emailAddress}`);
-    }
-  }
-  if(!sentOTPEmail) {
-    getOTPEmail();
-  }
-  
-  
-  return (
-    <>
-      {sentOTPEmail && 
-        <div className="container">
-          {!(otpError.length > 0) && <p>A One Time Pin was sent to {emailAddress}</p>}
-          {otpError.length > 0 && <div style={{color:'red', fontWeight:'bold', margin:'10px'}}>{otpError}</div>}
-          <form className="form-verify-otp" onSubmit={validateOTP}>
-            <label style={{width: "100%"}} htmlFor="otp">
-              Enter One Time Pin
-            </label>
-            <input className="input-field" id="otp" type="number" onChange={(e) => {if(e.target.value.length <= 6) setOneTimePin(e.target.value)}} value={oneTimePin}/>
-            
-            <button className="login-btn">Verify</button>
-          </form>
-        </div>}
-    </>
-  );
-}
-
-const ValidatingUsername = () => {
-
-  const [usernameError, setUsernameError] = useState("");
-  const [username, setUsername] = useState("");
-
-  const validUsername = async (e) => {
-    e.preventDefault();
-    const response = await fetch(`${APIURL}/validusername/${username}`)
-    const validated = await response.json();
-    if(validated.response) {
-      setValidatedUsername(true);
-      setGlobalUsername(username);
-    }
-    else {
-      setUsername("");
-      setUsernameError("Not Valid Username");
-    }
-  }
-
-  return (
-    <form className="form-fgt-pass" onSubmit={validUsername}>
-      {usernameError.length > 0 && <div style={{color:'red', fontWeight:'bold', margin:'10px'}}>{usernameError}</div>}
-        <label className="input-label" htmlFor="username">Username*</label>
-        <input className="input-field" id="usename" onChange={(e) => setUsername(e.target.value)} value={username}/>
-      <button className="login-btn">Forgot Password</button>
-    </form>
-  );
-}
-
-const ResetPassword = () => {
-  
+function ResetPassword(props) {
+  const navigate = useNavigate();
   const [newPassword, setNewPassword] = useState("");
   const [newPasswordConfirm, setNewPasswordConfirm] = useState("");
-  const [validatedPassword, setValidatedPassword] = useState(false);
   const [passwordError, setPasswordError] = useState("");
 
   const validPassword = async (e) => {
@@ -154,14 +24,14 @@ const ResetPassword = () => {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-          username: globalUsername,
+          username: props.globalUsername,
           newPassword: newPassword
         })
       });
       const res = await response.json()
       console.log(res);
       if(res.response) {
-        navigate("/");
+        navigate("/", {replace: true});
       }
       else {
         if(res.errcode === 1) {
@@ -193,16 +63,140 @@ const ResetPassword = () => {
   );
 }
 
+function ValidatingUsername(props) {
+
+  const [usernameError, setUsernameError] = useState("");
+  const [username, setUsername] = useState("");
+
+  const validUsername = async (e) => {
+    e.preventDefault();
+    const response = await fetch(`${APIURL}/validusername/${username}`)
+    const validated = await response.json();
+    if(validated.response) {
+      props.setValidatedUsername(true);
+      props.setGlobalUsername(username);
+    }
+    else {
+      setUsername("");
+      setUsernameError("Not Valid Username");
+    }
+  }
+
+  return (
+    <form className="form-fgt-pass" onSubmit={validUsername}>
+      {usernameError.length > 0 && <div style={{color:'red', fontWeight:'bold', margin:'10px'}}>{usernameError}</div>}
+        <label className="input-label" htmlFor="username">Username*</label>
+        <input className="input-field" id="usename" onChange={(e) => setUsername(e.target.value)} value={username}/>
+      <button className="login-btn">Forgot Password</button>
+    </form>
+  );
+}
+
+/*
+-Author: Mason Otto
+-Last Modified: 1/23/2023
+-Description: This is a React Component that is displayed to verify the OTP sent to the requesting user's email
+-Return: HTML Code that allows the user to enter the OTP
+TODO: Test code and refactor
+*/ 
+function VerifyOTP(props) {
+  const [otpError, setOTPError] = useState("");
+  const [oneTimePin, setOneTimePin] = useState("");
+
+  /*
+  -Author: Mason Otto
+  -Last Modified: 1/23/2023
+  -Description: This fetches to the backend API to generate a OTP and get the users email address
+  -Return: N/A
+  TODO: Test code and refactor
+  */ 
+  async function getOTPEmail(){
+    const response = await fetch(`${APIURL}/requestotp`, {
+      method: 'PUT',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: props.globalUsername
+      })
+    });
+    const sentEmail = await response.json();
+    if(sentEmail.response === "EMAIL SENT") {
+      props.setSentOTPEmail(true);
+      props.setEmailAddress(sentEmail.email);
+    }
+  }
+  /*
+  -Author: Mason Otto
+  -Last Modified: 1/23/2023
+  -Description: This fetches to the backend API to verify that the input OTP is correct
+  -Return: N/A
+  TODO: Test code and refactor
+  */ 
+  async function validateOTP(e) {
+    e.preventDefault();
+    const response = await fetch(`${APIURL}/verifyotp`, {
+      method: 'PUT',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: props.globalUsername,
+        otp: oneTimePin,
+      })
+    });
+    const verified = await response.json();
+    if(verified.response === "VERIFIED") {
+      props.setIsValidated(true);
+    }
+    else {
+      setOTPError(`Invalid One Time Pin! Check email sent to ${props.emailAddress}`);
+    }
+  }
+  if(!props.sentOTPEmail) {
+    getOTPEmail();
+  }
+  
+  
+  return (
+    <>
+      {props.sentOTPEmail && 
+        <div className="container">
+          {!(otpError.length > 0) && <p>A One Time Pin was sent to {props.emailAddress}</p>}
+          {otpError.length > 0 && <div style={{color:'red', fontWeight:'bold', margin:'10px'}}>{otpError}</div>}
+          <form className="form-verify-otp" onSubmit={validateOTP}>
+            <label style={{width: "100%"}} htmlFor="otp">
+              Enter One Time Pin
+            </label>
+            <input className="input-field" id="otp" type="number" onChange={(e) => {if(e.target.value.length <= 6) setOneTimePin(e.target.value)}} value={oneTimePin}/>
+            
+            <button className="login-btn">Verify</button>
+          </form>
+        </div>}
+    </>
+  );
+}
+/*-------------------------------------------------------------------------
+  Forgot Password Component
+-------------------------------------------------------------------------*/
+export default function ForgotPasswordScreen(){
+  const [validatedUsername, setValidatedUsername] = useState(false);
+  const [globalUsername, setGlobalUsername] = useState("");
+  const [isValidated, setIsValidated] = useState(false);
+  const [emailAddress, setEmailAddress] = useState("");
+  const [sentOTPEmail, setSentOTPEmail] = useState(false);
+
   /*-------------------------------------------------------------------------
     Forgot Password Screen
   -------------------------------------------------------------------------*/
-  //TODO: create and update specific to forgot password...
   return(
     <div className="container">
         <h1>Forgot Password</h1>
-        {validatedUsername === false && <ValidatingUsername />}
-        {validatedUsername === true && isValidated === true && <ResetPassword />}
-        {validatedUsername === true && isValidated === false && <VerifyOTP />}
+        {validatedUsername === false && <ValidatingUsername setGlobalUsername={setGlobalUsername} setValidatedUsername={setValidatedUsername}/>}
+        {validatedUsername === true && isValidated === true && <ResetPassword globalUsername={globalUsername}/>}
+        {validatedUsername === true && isValidated === false && <VerifyOTP globalUsername={globalUsername} emailAddress={emailAddress} setIsValidated={setIsValidated} setEmailAddress={setEmailAddress} sentOTPEmail={sentOTPEmail} setSentOTPEmail={setSentOTPEmail}/>}
     </div>
   )
     
