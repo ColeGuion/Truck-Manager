f55a23e033bd329f89787649cc1155bd727fb001
masonxotto <masonxotto@gmail.com>
2023-02-20 08:46:49 -0600

    values can be updated and state will update accordingly

--- TruckManager/src/Screens/UserProfileScreen.jsx
+++ TruckManager/src/Screens/UserProfileScreen.jsx
@@ -16,6 +16,12 @@ export default function UserProfileScreen({ navigation }){
   const [authenticated, setAuthenticated] = useState(true);
   const [updateInfo, setUpdateInfo] = useState(false);
 
+  /*
+    Author: Mason Otto
+    Creation Date: 2/20/2023
+    Last Modified: 2/20/2023
+    Description: This will run once on mount and will fetch all of the data for the current user
+  */
   useEffect(() => {
     async function fetchCurrentUser() {
         const response = await fetch(`${APIURL}/userdata`,{credentials: 'include'});
@@ -31,33 +37,63 @@ export default function UserProfileScreen({ navigation }){
     fetchCurrentUser();
 },[])
 
+  /*
+    Author: Mason Otto
+    Creation Date: 2/20/2023
+    Last Modified: 2/20/2023
+    Description: This will update the values in currentUser as values are changed
+  */
+  function handleChange(e) {
+    let copyOfUser = {...currentUser};
+    copyOfUser[e.target.id] = e.target.value;
+    setCurrentUser(currentUser => ({
+      ...copyOfUser
+    }));
+  }
+
   /*-------------------------------------------------------------------------
     User Profile Screen
   -------------------------------------------------------------------------*/
   return (
       <div className="page-container">
-        {!updateInfo &&
+        {updateInfo &&
         <div className="info-container">
-          <span className="info">Name: {(currentUser.firstName + " " + currentUser.lastName) || "N/A"}</span>
+          <span className="info">Name: {currentUser.name || "N/A"}</span>
           <span className="info">Employee ID: {currentUser.employeeId || "N/A"}</span>
           <span className="info">Email: {currentUser.email || "N/A"}</span>
+          <span className="info">Street: {currentUser.street || "N/A"}</span>
           <span className="info">State: {currentUser.state || "N/A"}</span>
           <span className="info">City: {currentUser.city || "N/A"}</span>
           <span className="info">Zipcode: {currentUser.zipcode || "N/A"}</span>
           <span className="info">Phone Number: {currentUser.phone || "N/A"}</span>
           <span className="info">Hire Date: {currentUser.hireDate || "N/A"}</span>
         </div>}
-        {updateInfo &&
+        {!updateInfo &&
+        <>
         <div className="info-container">
-          <input className="info"/>
-          <input className="info"/>
-          <input className="info"/>
-          <input className="info"/>
-          <input className="info"/>
-          <input className="info"/>
-          <input className="info"/>
-          <input className="info"/>
-        </div>}
+          <span className="info">Name:</span>
+          <span className="info">Employee ID:</span>
+          <span className="info">Email:</span>
+          <span className="info">Street:</span>
+          <span className="info">State:</span>
+          <span className="info">City:</span>
+          <span className="info">Zipcode:</span>
+          <span className="info">Phone Number:</span>
+          <span className="info">Hire Date:</span>
+        </div>
+        <div className="info-container">
+          <input className="info" id="name" onChange={handleChange} value={currentUser.name}/>
+          <input className="info" id="employeeId" onChange={handleChange} value={currentUser.employeeId}/>
+          <input className="info" id="email" onChange={handleChange} value={currentUser.email}/>
+          <input className="info" id="street" onChange={handleChange} value={currentUser.street}/>
+          <input className="info" id="state" onChange={handleChange} value={currentUser.state}/>
+          <input className="info" id="city" onChange={handleChange} value={currentUser.city}/>
+          <input className="info" id="zipcode" onChange={handleChange} value={currentUser.zipcode}/>
+          <input className="info" id="phone" onChange={handleChange} value={currentUser.phone}/>
+          <input className="info" id="hireDate" value={currentUser.hireDate}/>
+        </div>
+        </>
+        }
       </div>
   );
 
