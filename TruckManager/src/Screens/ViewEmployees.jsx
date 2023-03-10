2137dc4c18368c96d6647fa48f7b495d778d3436
masonxotto <masonxotto@gmail.com>
2023-03-09 19:43:21 -0600

    added searching by Name and Employee_ID to the ViewEmployees (built off of ryans implementation for tickets)

--- TruckManager/src/Screens/ViewEmployees.jsx
+++ TruckManager/src/Screens/ViewEmployees.jsx
@@ -10,8 +10,49 @@ import API_URL from'../config.json';
 
 const APIURL = API_URL.API_URL;
 
+function FilterOptions(props) {
+
+    const handleFilterOptionChange = (e) => {
+        props.setFilterOption(e.target.value);
+    }
+
+    return (
+        <div className="filter-options">
+        <label htmlFor="search">
+            Search by:
+        </label>
+        <form>
+            <label className="filter-radio-button" htmlFor="name">
+            Name
+            <input
+                type="radio"
+                name="filterOption"
+                value="Name"
+                id="name"
+                checked={props.filterOption === "Name"}
+                onChange={handleFilterOptionChange}
+            />
+            </label>
+            <label className="filter-radio-button" htmlFor="employee_id">
+            Employee ID
+            <input
+                type="radio"
+                name="filterOption"
+                value="Employee_ID"
+                id="employee_id"
+                checked={props.filterOption === "Employee_ID"}
+                onChange={handleFilterOptionChange}
+            />
+            </label>
+        </form>
+        </div>
+    );
+}
+
 export default function ViewEmployees() {
     const [employees, setEmployees] = useState([{}]);
+    const [filterOption, setFilterOption] = useState("Name");
+    const [filterSearch, setFilterSearch] = useState("");
 
     useEffect(() => {
         async function fetchEmployees() {
@@ -25,7 +66,19 @@ export default function ViewEmployees() {
     return (
         <div className="page-container">
             <h1>Employees</h1>
-            {employees.map((employee, idx) => {
+            <FilterOptions filterOption={filterOption} setFilterOption={setFilterOption}/>
+            <input
+                type="text"
+                placeholder="Search..."
+                className="filter-search"
+                id="search"
+                onChange={(e) => setFilterSearch(e.target.value)}
+                value={filterSearch}
+            />  
+            {employees.filter((employee) =>
+          employee[filterOption]?.toString().includes(filterSearch))
+          .sort( (a,b) => {return parseInt(a[filterOption], 10) > parseInt(b[filterOption], 10) ? 1 : -1} )
+          .map((employee, idx) => {
                 return (
                     <div className="info-container" key={idx}>
                         <span className="info">Name:  {employee.Name || "N/A"}</span>
